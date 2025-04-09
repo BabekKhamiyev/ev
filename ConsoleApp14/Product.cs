17bc80471b5e222b81d8682eb87eeddb06267f1c
aysuhsn <hsnovaaysu20@gmail.com>
2025-04-09 19:16:31 +0400

    FD

---
+++ ConsoleApp14/Product.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14;
+
+public class Product
+{
+    private static int _no;
+    public int No;
+
+    public string Name;
+	private double price;
+    public Profession profession;
+    public double Price
+    {
+		get { return price; }
+		
+        set
+        { 
+            if (value > 0)
+            {
+                price = value;
+            }
+        }
+	}
+
+    public Product(string name, double price,Profession profession)
+    {
+        _no++;
+        No=_no;
+        Name = name;
+        Price = price;
+        this.profession = profession;
+    }
+    public enum Profession
+    {
+        Baker, 
+        Drink,
+        Meat, 
+        Diary
+    }
+}
---
+++ ConsoleApp14/Store.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConsoleApp14.Product;
+
+namespace ConsoleApp14;
+
+public class Store
+{
+
+    Product[] products = new Product[0];
+    public void AddProduct(Product product)
+    {
+        Array.Resize(ref products, products.Length + 1);
+        products[^1] = product;
+        Console.WriteLine("elave olundu");
+    }
+
+
+
+    public Product GetProduct(int no)
+    {
+        foreach (Product p in products)
+        {
+            if (p.No == no)
+            {
+                return p;
+            }
+        }
+        return default;
+    }
+
+    public Product[] FilterProductsByType(Profession prof)
+    {
+        Product[] pro = new Product[0];
+
+        foreach (Product p in products)
+        {
+            if (p.profession == prof)
+            {
+                Array.Resize(ref pro, pro.Length + 1);
+                products[^1] = p;
+            }
+        }
+        return pro;
+    }
+
+    public Product[] FilterProductByName(string name)
+    {
+        Product[] pro = new Product[0];
+
+        foreach (Product p in products)
+        {
+            if (p.Name == name)
+            {
+                Array.Resize(ref pro, pro.Length + 1);
+                products[^1] = p;
+            }
+        }
+        return pro;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
